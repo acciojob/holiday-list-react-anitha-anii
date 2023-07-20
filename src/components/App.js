1df7e6e289c7823a66c663a3a95bc3dfd39221a3
anitha-anii <anithachowdary355@gmail.com>
2023-07-20 15:09:53 +0530

    'changes'

--- src/components/App.js
+++ src/components/App.js
@@ -33,11 +33,19 @@ const App = () => {
     { name: 'Tirupati', country: 'India' },
     ]
     
-  return (
-    <div id="main">
-               {/* Do not remove the main div */}
-    </div>
-  )
+    const indianCities = cityList.filter((city) => city.country === 'India');
+
+    return (
+      <div id="main">
+        <h1>Top Indian Vacation Cities</h1>
+        <ol>
+          {indianCities.map((city, index) => (
+            <li key={`location${index + 1}`}>{city.name}</li>
+          ))}
+        </ol>
+      </div>
+    );
+  
 }
 
 export default App
